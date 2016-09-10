c452fd97f4d93b5b4b18e9750b179257bf2159e3
superkevinigt <superkevingit@gmail.com>
2016-09-10 17:20:06 +0800

    init

---
+++ app/Http/Controllers/WechatController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class WechatController extends Controller
+{
+    public function serve()
+    {
+        Log::info('request arrived.');
+
+        $wechat = app('wechat');
+        $wechat->server->setMessageHandler(function($message){
+            return "欢迎关注 日新微信!";
+        });
+
+        Log::info('return response.');
+
+        return $wechat->server->serve();
+    }
+}
