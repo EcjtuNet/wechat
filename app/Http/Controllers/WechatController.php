24dd0134997915b7a71739444b1815ff1ba98cc8
ABOTGAO <lettimego@gmail.com>
2017-02-16 23:20:01 -0600

    整理代码

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use EasyWeChat\Foundation\Application;
 use App\Http\Controllers\WechatTextController;
+use App\Http\Controllers\UserController;
 
 class WechatController extends Controller
 {
@@ -11,9 +12,32 @@ class WechatController extends Controller
     {
         $wechat = app('wechat');
         $userApi = $wechat->user;
-        $wechat->server->setMessageHandler(function($message) use ($userApi){
+        $wechat->server->setMessageHandler(function($message) use ($userApi) {
             switch ($message->MsgType) {
                 case 'event':
+                    if($message->Event == 'subscribe')
+                    {
+                        return '欢迎你订阅日小新公众号，在这里你可以很方便的查询自己的成绩，课表以及考试安排。请先绑定学号才能查询成绩。';
+                    }
+
+                    if($message->Event == 'CLICK')
+                    {
+                        if($message->EventKey == 'Bound')
+                        {
+                            return (new UserController())->firstmeet();
+                        }
+                        elseif($message->EventKey == 'Class')
+                        {
+                            return (new WechatTextController())->distinguishText('成绩',$message->FromUserName);
+                        }
+                        elseif ($message->EventKey == 'Score') 
+                        {
+                            return (new WechatTextController())->distinguishText('课表',$message->FromUserName);
+                        }
+                        elseif ($message->EventKey == 'Exam') {
+                            return (new WechatTextController())->distinguishText('考试安排',$message->FromUserName);
+                        }
+                    }
                     break;
                 case 'text':
                     return (new WechatTextController())->distinguishText($message->Content, $message->FromUserName);
