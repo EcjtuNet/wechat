201c0655789522dbf7282289b7951cf92bb00eed
ABOTGAO <lettimego@gmail.com>
2017-02-16 23:43:25 -0600

    修改job为可队列，显示课表

--- app/Jobs/sendClass.php
+++ app/Jobs/sendClass.php
@@ -2,26 +2,29 @@
 
 namespace App\Jobs;
 
+use App\Http\Controllers\debugController;
 use App\Jobs\Job;
+use EasyWeChat\Message\Text;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Bus\SelfHandling;
 use Illuminate\Contracts\Queue\ShouldQueue;
-use Overtrue\Socialite\Providers\WeChatProvider;
-use EasyWeChat\Foundation\Application;
+use EasyWeChat;
 
 class sendClass extends Job implements SelfHandling, ShouldQueue
 {
     use InteractsWithQueue, SerializesModels;
     public $sender;
+    public $class;
     /**
      * Create a new job instance.
      *
      * @return void
      */
-    public function __construct($sender)
+    public function __construct($sender,$class)
     {
         $this->sender = $sender;
+        $this->class  = $class;
     }
 
     /**
@@ -31,7 +34,20 @@ class sendClass extends Job implements SelfHandling, ShouldQueue
      */
     public function handle()
     {
-        $message = new Text(['content' => '你的课表']);
+        // $message = new News([
+        //       'title'  =>  '你的课表',
+        //       'image'  =>  'http://mmbiz.qpic.cn/mmbiz_jpg/J1iaWB1VlOykPibTacZHD7IvibkH7VH88vUMJlwvMOxqT0aHvFILOlkMYibxlErZD1osvTEb9QdaCtn2ibw15t2lq7g/0?wx_fmt=jpeg',
+        //       'description' => '点击查看课表',
+
+        //     ]);
+        $week = date("l");
+        $week = lcfirst($week);
+        $msg = "你今天的课表 \n===========\n";
+        foreach ($this->class['data']['class_list']["$week"] as $class) {
+            $msg = $msg . $class . "\n";
+        }
+
+        $message = new Text(['content' => $msg]);
         EasyWeChat::staff()->message($message)->to($this->sender)->send();
     }
 }
