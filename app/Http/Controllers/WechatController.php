5d04fc4fd9f2002e7b863ad45f5b9336ed33cd53
ABOTGAO <lettimego@gmail.com>
2017-07-07 14:18:36 +0800

    修改回复

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use EasyWeChat\Message\Text;
+use EasyWeChat;
 use EasyWeChat\Foundation\Application;
 use App\Http\Controllers\WechatTextController;
 use App\Http\Controllers\UserController;
@@ -17,21 +19,22 @@ class WechatController extends Controller
                 case 'event':
                     if($message->Event == 'subscribe')
                     {
-                        return '/:rose谢谢你对交大的支持！我们是你的好伙伴，将为你带来新鲜好玩的校园新闻，有用的资讯信息和贴心的生活服务/:sun！对我们有任何意见或建议都可以直接发送给我们，我们会及时给您回复！欢迎提供校园新闻线索，有什么新奇想法有趣图文也可以投稿给我们/:lvu！';
+                        $content = new Text(['content' => "/:rose欢迎关注华东交通大学，谢谢你对交大的支持！我们是你的好伙伴，将为你带来新鲜好玩的校园新闻，有用的资讯信息和贴心的生活服务！对我们有任何意见或建议都可以直接发送给我们，我们会及时给您回复！欢迎提供校园新闻线索，有什么新奇想法有趣图文也可以投稿给我们！请加入我们的17迎新群642351822/:lvu！"]);
+                        EasyWeChat::staff()->message($content)->to($message->FromUserName)->send();
+                        // return '/:rose谢谢你对交大的支持！我们是你的好伙伴，将为你带来新鲜好玩的校园新闻，有用的资讯信息和贴心的生活服务/:sun！对我们有任何意见或建议都可以直接发送给我们，我们会及时给您回复！欢迎提供校园新闻线索，有什么新奇想法有趣图文也可以投稿给我们/:lvu！';
                     }
 
                     if($message->Event == 'CLICK')
                     {
-                        // if ($message->EventKey == 'Band') {
-                        //     //return (new WechatTextController())->loadView($message->FromUserName);
-                        //     return (new WechatTextController())->boundStudentId($message->FromUserName);
-                        //     //return view('welcome');
-                        // }
                         if ($message->EventKey == 'Query') {
-                            return '/:8-) 校内查询功能即将上线';
+                            $content = new Text(['content' => "/:8-) 校内查询功能即将上线"]);
+                            EasyWeChat::staff()->message($content)->to($message->FromUserName)->send();
+                            //return '/:8-) 校内查询功能即将上线';
                         }
                         elseif ($message->EventKey == 'LostAndFound') {
-                            return '失物招领功能即将上线，届时将为大家提供平台，把丢失的饭卡速速的找回来/:8-)';
+                            $content = new Text(['content' => "失物招领功能即将上线，届时将为大家提供平台，把丢失的饭卡速速的找回来/:8-)"]);
+                            EasyWeChat::staff()->message($content)->to($message->FromUserName)->send();
+                            //return '失物招领功能即将上线，届时将为大家提供平台，把丢失的饭卡速速的找回来/:8-)';
                         }
                     }
 
